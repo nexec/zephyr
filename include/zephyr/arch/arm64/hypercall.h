4f520ececebe0947ad638087497b4486cab968a8
Dmytro Firsov <dmytro_firsov@epam.com>
2021-11-11 13:22:56 +0200

    xenvm: dom0: PoC for booting Zephyr guest from Zephyr Dom0
    This commit implements example of guest creation in Zephyr as Dom0.
    Now one instanse of DomU Zephyr is added to main Zephyr Dom0 binary
    as .zephyr_domu section (file is located in include/xen/dom0/
    directory). These binaries are compiled for different RAM start
    addresses - Domu is compiled for usual GUEST_RAM0_BASE, Dom0 is
    compiled for address, that was set for domain 0 by Xen (can be found
    in Xen bootlog).
    
    After Dom0 Zephyr boot it creates new domain via domctl, performs
    configuration, populates memory, maps it and loads domain binary.
    All steps are pretty close to libxl creation sequence. Configs are now
    defined in dom0.c file. After domain_unpause() DomU start to print to
    consoleio, so to see this you need to have debug Xen build.
    
    To build Dom0 binary I am using shell sample:
    
      $: west build -b xenvm samples/subsys/shell/shell_module
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>
    (cherry picked from commit d07342599c53a7398c4019767f9ade709d5a294e)

--- include/zephyr/arch/arm64/hypercall.h
+++ include/zephyr/arch/arm64/hypercall.h
@@ -11,3 +11,5 @@ int HYPERVISOR_event_channel_op(int op, void *param);
 int HYPERVISOR_hvm_op(int op, void *param);
 int HYPERVISOR_memory_op(int op, void *param);
 int HYPERVISOR_grant_table_op(int op, void *uop, unsigned int count);
+int HYPERVISOR_domctl(void *param);
+int HYPERVISOR_sysctl(void *param);
